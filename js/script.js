f2c2e11702da24ae16943d237eed84cce9a1eadb
Hycarus <mirkomarasco08@gmail.com>
2023-10-12 16:02:22 +0200

    added validation mail

--- js/script.js
+++ js/script.js
@@ -13,26 +13,36 @@ resetBtn.addEventListener('click', function(){
 const button = document.querySelector('button');
 button.addEventListener('click', function(){
     let found = false;
+    let unvalid = false;
     let insertAddress = document.getElementById('address').value;
-    console.log(insertAddress);
-    for(let i = 0; i < mailAddress.length; i++){
-    let currentAddress = mailAddress[i];
-        if(currentAddress === insertAddress){
-            found = true;
-        }
-    }    
     let container = document.querySelector('.container');
-    if(found === true){
-        let myAlert = document.createElement('div');
-        myAlert.className = `bg-success rounded p-3`;
-        myAlert.innerHTML = `Email trovata!`;
-        container.append(myAlert);
-        console.log(myAlert);
-    } else{
+    console.log(insertAddress);
+    if(insertAddress.includes('@') === unvalid){
         let myAlert = document.createElement('div');
         myAlert.className = `bg-danger rounded p-3`;
-        myAlert.innerHTML = `Email non trovata!`;
+        myAlert.innerHTML = `Indirizzo email non valido!`;
         container.append(myAlert);
-        console.log(myAlert);
+    } else{
+        for(let i = 0; i < mailAddress.length; i++){
+            let currentAddress = mailAddress[i];  
+            if (currentAddress === insertAddress){
+                found = true;
+            }    
+        }
+        if(found === true){
+            let myAlert = document.createElement('div');
+            myAlert.className = `bg-success rounded p-3`;
+            myAlert.innerHTML = `Email trovata!`;
+            container.append(myAlert);
+            console.log(myAlert);
+        } else{
+            let myAlert = document.createElement('div');
+            myAlert.className = `bg-danger rounded p-3`;
+            myAlert.innerHTML = `Email non trovata!`;
+            container.append(myAlert);
+            console.log(myAlert);
+        }
     }
+    
+    
 })
